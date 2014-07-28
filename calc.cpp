c52fa85a5e485565f375030c17fdead555cb5b0a
soumyap99 <soumya.pandey@cumminscollege.in>
2014-07-28 11:08:19 +0530

    Update calc.cpp
    included add function

--- calc.cpp
+++ calc.cpp
@@ -1,5 +1,12 @@
 #include<iostream.h>
 void main()
 {
-  cout<<"hello github";
+  cout<<"welcome to github\n";
+
+}
+
+
+double add(double in_number1,double in_number2)
+{
+  return in_number1+in_number2;
 }
